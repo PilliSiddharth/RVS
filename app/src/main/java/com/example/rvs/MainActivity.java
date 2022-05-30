f0f60913204519bfc853d2a605915360d80fe28b
IL09 <Siddharth.Pilli@infinitylearn.com>
2022-05-30 14:05:38 +0530

    Created RVS Repo

---
+++ app/src/main/java/com/example/rvs/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.rvs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("Rapid Visual Screening (RVS)");
+
+    }
+}
