c2597e10da56a906061e4e3c1c6da361f0904980
IL09 <Siddharth.Pilli@infinitylearn.com>
2022-09-07 15:46:10 +0530

    latitude and longitude auto selects

--- app/src/main/java/com/example/rvs/MainActivity.java
+++ app/src/main/java/com/example/rvs/MainActivity.java
@@ -3,14 +3,18 @@ package com.example.rvs;
 import static android.content.ContentValues.TAG;
 
 import androidx.annotation.IdRes;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.FileProvider;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -18,11 +22,15 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Looper;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -36,6 +44,13 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
@@ -51,6 +66,7 @@ import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 import com.memorynotfound.pdf.itext.WatermarkPageEvent;
+
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -71,6 +87,11 @@ import java.util.Set;
 public class MainActivity<TimeActivity> extends AppCompatActivity {
 
     final Calendar myCalendar = Calendar.getInstance();
+
+    FusedLocationProviderClient mFusedLocationClient;
+    int PERMISSION_ID = 44;
+    String user_latitude;
+    String user_longitude;
 
     File myFile;
     File pdfFolder;
@@ -176,6 +197,10 @@ public class MainActivity<TimeActivity> extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().setTitle("Rapid Visual Screening (RVS)");
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        getLastLocation();
+
         inspector_id = findViewById(R.id.inspector_id);
         datepicker = findViewById(R.id.datepicker);
         chooseTime = findViewById(R.id.timepicker);
@@ -981,6 +1006,9 @@ public class MainActivity<TimeActivity> extends AppCompatActivity {
 //                lifeline_data.clear();
 //                commercial_data.clear();
                 roof_data.clear();
+
+                System.out.println("Latitude: "+user_latitude);
+                System.out.println("Longitude" +user_longitude);
 //                office_data.clear();
 //                mixed_data.clear();
 //                industrial_data.clear();
@@ -990,8 +1018,116 @@ public class MainActivity<TimeActivity> extends AppCompatActivity {
         });
     }
 
+
+
 //    Drawable customErrorDrawable = getResources().getDrawable(R.drawable.ic_baseline_error_outline_24);
 //    customErrorDrawable.setBounds(0, 0, customErrorDrawable.getIntrinsicWidth(), customErrorDrawable.getIntrinsicHeight());
+    @SuppressLint("MissingPermission")
+    private void getLastLocation() {
+    // check if permissions are given
+    if (checkPermissions()) {
+
+        // check if location is enabled
+        if (isLocationEnabled()) {
+
+            // getting last
+            // location from
+            // FusedLocationClient
+            // object
+            mFusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                @Override
+                public void onComplete(@NonNull Task<Location> task) {
+                    Location location = task.getResult();
+                    if (location == null) {
+                        requestNewLocationData();
+                    } else {
+                       user_latitude = location.getLatitude() + "";
+                       user_longitude = location.getLongitude() + "";
+                    }
+                }
+            });
+        } else {
+            Toast.makeText(this, "Please turn on" + " your location...", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+        }
+    } else {
+        // if permissions aren't available,
+        // request for permissions
+        requestPermissions();
+    }
+}
+
+    @SuppressLint("MissingPermission")
+    private void requestNewLocationData() {
+
+        // Initializing LocationRequest
+        // object with appropriate methods
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(5);
+        mLocationRequest.setFastestInterval(0);
+        mLocationRequest.setNumUpdates(1);
+
+        // setting LocationRequest
+        // on FusedLocationClient
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+    }
+
+    private LocationCallback mLocationCallback = new LocationCallback() {
+
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            Location mLastLocation = locationResult.getLastLocation();
+            user_latitude = "Latitude: " + mLastLocation.getLatitude() + "";
+            user_longitude = "Longitude: " + mLastLocation.getLongitude() + "";
+        }
+    };
+
+    private boolean checkPermissions() {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+
+        // If we want background location
+        // on Android 10.0 and higher,
+        // use:
+        // ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+
+    // method to request for permissions
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_ID);
+    }
+
+    // method to check
+    // if location is enabled
+    private boolean isLocationEnabled() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    // If everything is alright then
+    @Override
+    public void
+    onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSION_ID) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastLocation();
+            }
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (checkPermissions()) {
+            getLastLocation();
+        }
+    }
 
 
     private void updateLabelDate() {
